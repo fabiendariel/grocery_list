d60acc498b79cf9ccfc4819871016119de070a2d
Fabien Dariel <fabien.dariel@gmail.com>
2024-05-01 17:45:18 -0400

    Application to manipulate a grocery list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import AddItem from './AddItem';
+import SearchItem from './SearchItem';
+import Content from './Content';
+import Footer from './Footer';
+import { useState, useEffect } from 'react'
+import apiRequest from './apiRequest';
 
 function App() {
+  const API_URL = 'http://localhost:3500/items';
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if(!response.ok) throw Error('No data');
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setisLoading(false);
+      }
+    }
+    setTimeout(() => {
+      fetchItems();
+    }, 2000)
+    
+  }, [items])
+
+  const addItem = async (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const thisNewItem = { id, checked: false, item };
+    const listItems = [...items, thisNewItem];
+    setItems(listItems);
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(thisNewItem)
+    };
+    const result = await apiRequest(API_URL, postOptions);
+    if(result) setFetchError(result);
+  }
+
+  const handleCheck = async (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItems);
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ checked: myItem[0].checked })
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
+  };
+
+  const handleDelete = async (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+
+    const deleteOptions = {method: 'DELETE'};
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!newItem) return;
+    addItem(newItem);
+    setNewItem('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="Grocery List" /> 
+      <AddItem 
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      /> 
+      <main>
+        {isLoading && <p>Loading items...</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && <Content 
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />}
+      </main>
+      <Footer length={items.length} /> 
     </div>
   );
 }
---
+++ src/SearchItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SearchItem = ({ search, setSearch }) => {
+  return (
+    <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
+      <label htmlFor="search">Search</label>
+      <input
+        id='search'
+        type="text"
+        placeholder='Search Items'
+        role='searchbox'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </form>
+  )
+}
+
+export default SearchItem
